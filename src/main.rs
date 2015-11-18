d44275a68370f463c3d8e8c81e5d792a47ca824a
David Avedissian <git@davedissian.com>
2015-11-18 22:24:36 +0000

    Implemented dword memory functions and split map into read/write

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,7 @@ struct Memory {
     bank: [u8; 8192],
     internal: [u8; 8192],
     oam: [u8; 160],
-    ier: u8,
-    dummy: u8
+    ier: u8
 }
 
 impl Memory {
@@ -38,28 +37,21 @@ impl Memory {
             bank: [0u8; 8192],
             internal: [0u8; 8192],
             oam: [0u8; 160],
-            ier: 0,
-            dummy: 0
+            ier: 0
         }
     }
 
-    fn map(&mut self, addr: u16) -> &mut u8 {
-        match addr {
-            0x8000 ... 0x9FFF => &mut self.vram[addr as usize - 0x8000],
-            0xA000 ... 0xBFFF => &mut self.bank[addr as usize - 0xA000],
-            0xC000 ... 0xDFFF => &mut self.internal[addr as usize - 0xC000],
-            0xE000 ... 0xFDFF => &mut self.internal[addr as usize - 0xE000],
-            0xFE00 ... 0xFE9F => &mut self.oam[addr as usize - 0xFE00],
-            0xFFFF => &mut self.ier,
-            _ => {
-                println!("ERROR: Out of bounds memory write");
-                &mut self.dummy
-            } // TODO: error
-        }
-    }
-
+    // Memory Writing
     fn write(&mut self, addr: u16, value: u8) {
-        *self.map(addr) = value;
+        match addr {
+            0x8000 ... 0x9FFF => self.vram[addr as usize - 0x8000] = value,
+            0xA000 ... 0xBFFF => self.bank[addr as usize - 0xA000] = value,
+            0xC000 ... 0xDFFF => self.internal[addr as usize - 0xC000] = value,
+            0xE000 ... 0xFDFF => self.internal[addr as usize - 0xE000] = value,
+            0xFE00 ... 0xFE9F => self.oam[addr as usize - 0xFE00] = value,
+            0xFFFF            => self.ier = value,
+            _ => panic!("ERROR: Out of bounds memory write")
+        }
     }
 
     fn write_word(&mut self, addr: u16, word: u16) {
@@ -67,20 +59,43 @@ impl Memory {
         self.write(addr + 1, ((word >> 8) & 0xFF) as u8);
     }
 
-    // TODO: Make these not mutable
-    fn read(&mut self, addr: u16) -> u8 {
-        return *self.map(addr);
+    fn write_dword(&mut self, addr: u16, dword: u32) {
+        self.write(addr + 0, ((dword >>  0) & 0xFF) as u8);
+        self.write(addr + 1, ((dword >>  8) & 0xFF) as u8);
+        self.write(addr + 2, ((dword >> 16) & 0xFF) as u8);
+        self.write(addr + 3, ((dword >> 24) & 0xFF) as u8);
     }
 
-    fn read_word(&mut self, addr: u16) -> u16 {
+    // Memory Reading
+    fn read(&self, addr: u16) -> u8 {
+        match addr {
+            0x8000 ... 0x9FFF => self.vram[addr as usize - 0x8000],
+            0xA000 ... 0xBFFF => self.bank[addr as usize - 0xA000],
+            0xC000 ... 0xDFFF => self.internal[addr as usize - 0xC000],
+            0xE000 ... 0xFDFF => self.internal[addr as usize - 0xE000],
+            0xFE00 ... 0xFE9F => self.oam[addr as usize - 0xFE00],
+            0xFFFF            => self.ier,
+            _ => panic!("ERROR: Out of bounds memory read")
+        }
+    }
+
+    fn read_word(&self, addr: u16) -> u16 {
         return
             ((self.read(addr + 0) as u16) << 0) +
             ((self.read(addr + 1) as u16) << 8)
+    }
+
+    fn read_dword(&self, addr: u16) -> u32 {
+        return
+            ((self.read(addr + 0) as u32) <<  0) +
+            ((self.read(addr + 1) as u32) <<  8) +
+            ((self.read(addr + 2) as u32) << 16) +
+            ((self.read(addr + 3) as u32) << 24)
     }
 }
 
 fn main() {
     let mut memory = Memory::new();
-    memory.write_word(0x8010, 32);
-    println!("{}", memory.read_word(0x8010));
+    memory.write_dword(0x8000, 32);
+    println!("{}", memory.read_dword(0x8000));
 }
